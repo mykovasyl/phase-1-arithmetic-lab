243e4d2581ac6fdd6aeb531fc4bf2e1d7243c3c7
Mykola Vasyl'yev <mykovasyl@gmail.com>
2021-11-01 15:25:14 -0400

    commit tree 7abd674d33368aa83445f114abd5df8bb778accb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.round(Math.random() * 100);
+
+const num3 = 16;
+const num4 = 12;
+
+const mod = num3 % num4;
+
+const max = Math.max(5, 7, 10, 14, 20);
